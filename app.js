deb141c9358d9336c9c821e625333141c5c7840b
Daniela Dimova <180798640+StoicDaniela@users.noreply.github.com>
2025-10-20 02:50:02 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// users database (in real app this would be on server)
+// Demo users database (in real app this would be on server)
 const users = {
     'admin@stoic11.com': {
         password: 'admin123',
